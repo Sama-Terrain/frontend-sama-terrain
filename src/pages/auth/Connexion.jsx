ae73cab5d35f5576755f351679c186f5558f700e
anf692 <fassane348@gmail.com>
2026-09-21 13:04:07 +0000

    correction de la langue

--- src/pages/auth/Connexion.jsx
+++ src/pages/auth/Connexion.jsx
@@ -185,7 +185,7 @@ export default function Connexion() {
                   d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z"
                 />
               </svg>
-              <span>Sign in with Google</span>
+              <span>S'incrire avec Google</span>
             </div>
             <div
               ref={googleBtnRef}
@@ -195,12 +195,12 @@ export default function Connexion() {
 
           {/* Lien vers Inscription */}
           <p className="text-center text-xs text-gray-500 pt-4">
-            Don't you have an account?{' '}
+            Vous n'avez pas de compte ?{' '}
             <button
               onClick={() => navigate('/register')}
               className="text-[#D4AF37] font-bold hover:underline cursor-pointer"
             >
-              Sign up
+              S'incrire
             </button>
           </p>
 
